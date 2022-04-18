4262d4d2291c796afc6bb28bc44b1959d6f5430e
Barld Boot <barld.boot@coas.com>
2022-04-18 21:33:55 +0200

    Add project files.

---
+++ PrimeNumbers/PrimeNumbers/PrimeCalculator.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimeNumbers
+{
+    public class PrimeCalculator
+    {
+        public static IEnumerable<int> PrimesBelow(int n)
+        {
+            var primes = new List<int> { 2, 3 };
+            for(int i = 5; i < n; i += 2)
+            {
+                var sqrtI = Math.Sqrt(i);
+                var isPrime = true;
+                for (int j = 3; j < sqrtI; j+= 2)
+                {
+                    if (i%j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+        }
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers/wwwroot/scripts/prime.js
@@ -0,0 +1,29 @@
+﻿function getPrimesBelow(n) {
+    const primes = [2, 3]
+    for (let i = 5; i < n; i+= 2) 
+    {
+        const sqrtI = Math.sqrt(i);
+        let isPrime = true;
+        for (let j = 3; j < sqrtI; j += 2)
+        {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            primes.push(i);
+        }
+    }
+
+    return primes;
+}
+
+window.performTest = function (n) {
+    let t0 = performance.now();
+    let numbersCount = getPrimesBelow(n).length;
+    let t1 = performance.now();
+
+    return `Amount: ${numbersCount}. within: ${t1 - t0} ms.`;
+}
